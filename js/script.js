f3d97e149c3bcfe0d14a6d69e67907da359b5981
Sotchi <sovanpisothyan@example.com>
2025-12-19 20:36:46 +0700

    For Working on Dashboard

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ const navDrop = document.querySelector(".navDrop");
 hamburger.addEventListener("click", () => {
     navDrop.classList.toggle("open");
 });
+
+const menu = document.getElementById("menu");
+const sidebar = document.getElementById("sidebar");
+
+menu.addEventListener("click", () => {
+    sidebar.classList.toggle("-translate-x-full");
+});
+
+
